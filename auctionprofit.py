8c2f1950a8058ff2f50df507a00be2dc54ea1bcf
mr-hespicable <leonhmcqueen@gmail.com>
2022-12-29 22:50:12 +0000

    Update auctionprofit.py
    refined & added more functions to ovr make the code cleaner & easier to read

--- auctionprofit.py
+++ auctionprofit.py
@@ -1,4 +1,3 @@
-import gzip
 import time
 import io
 import requests
@@ -6,66 +5,70 @@ import json
 import nbt
 import base64
 
-
+timesLooped = 0
 def getInfo(call):
     r = requests.get(call)
-    return r.json
+    return r.json()
 def decode_inventory_data(raw_data):
-       data = nbt.nbt.NBTFile(fileobj = io.BytesIO(base64.b64decode(raw_data)))
-       extra = data.tags[0].tags[0].__getitem__('tag').__getitem__('ExtraAttributes')
-       id = extra.__getitem__('id')
-       print(id)
-               
+    data = nbt.nbt.NBTFile(fileobj=io.BytesIO(base64.b64decode(raw_data)))
+    extra = data.tags[0].tags[0]['tag']['ExtraAttributes']
+    id = extra['id']
+    print(id)
+#cred. ShadowMobX#0220 on discord
        
+
+USERNAME = input('say username here please\n')
+API_KEY = input('say api key here pls\n')
+mojang = f"https://api.mojang.com/users/profiles/minecraft/{USERNAME}?"
+response = requests.get(mojang)
+UUID = response.json()['id']
+profile = f"https://api.hypixel.net/player?key={API_KEY}&uuid={UUID}"
+PROFILEUUID = profile['player']['stats']['SkyBlock']['profiles']['caccc92b1ecd43998a30901953029379']
+print(PROFILEUUID)
+
+
+auctionsUp = f"https://api.hypixel.net/skyblock/auction?key={API_KEY}&profile={PROFILEUUID}"
+auctionResponse = requests.get(auctionsUp)
+data = auctionResponse.text
+time.sleep(1)
+
+parse_json = json.loads(data)      
+auctionList = parse_json['auctions']
+for auctionItem in auctionList:
+    sold = auctionItem['highest_bid_amount']
+    if sold == 0:
+        starting_bid = auctionItem['starting_bid'] 
+        item_name = auctionItem['item_name']
+        print(f'{item_name} costs {starting_bid}. Claimed: {sold}\n')
+
+auctionsSold = f"https://api.hypixel.net/skyblock/auctions_ended?key-{API_KEY}"
+soldauctionResponse = requests.get(auctionsSold)
+soldData = soldauctionResponse.text
+parse_json = json.loads(soldData)
+soldauctionList = parse_json['auctions']
+time.sleep(1)
+    
 while True:
-    USERNAME = 'websafe'
-    API_KEY = "4c24c4b5-ad52-4bb9-ac0e-d506a1a8793a"
-    mojang = f"https://api.mojang.com/users/profiles/minecraft/{USERNAME}?"
-    response = requests.get(mojang)
-    UUID = response.json()['id']
-    PROFILEUUID = "caccc92b1ecd43998a30901953029379"
-
-
-    auctionsUp = f"https://api.hypixel.net/skyblock/auction?key={API_KEY}&profile={PROFILEUUID}"
-    auctionResponse = requests.get(auctionsUp)
-    data = auctionResponse.text
-
-
-    x = 1
-
-    time.sleep(1)
-    parse_json = json.loads(data)
-
-    n = 0        
-    auctionList = parse_json['auctions']
-
-    for auctionItem in auctionList:
-        sold = auctionItem['highest_bid_amount']
-        if sold == 0:
-            starting_bid = auctionItem['starting_bid'] 
-            item_name = auctionItem['item_name']
-            
-            print(f'{item_name} costs {starting_bid}. Claimed: {sold}\n')
-
-    auctionsSold = f"https://api.hypixel.net/skyblock/auctions_ended?key-{API_KEY}"
-    soldauctionResponse = requests.get(auctionsSold)
-    soldData = soldauctionResponse.text
-    parse_json = json.loads(soldData)
-    soldauctionList = parse_json['auctions']
-    time.sleep(1)
-    
-    
-        
     for soldItem in soldauctionList:
         uuid = soldItem['buyer']
-        if uuid == '2e82ec5b72354497adfde07170979a72':
-            gzipped_data = soldItem['item_bytes']
-            decode_inventory_data(gzipped_data)
-            exit()
-            
-            
-        else:
-            print('No matching auctions found.')
-    time.sleep(15)
-    print('halfway')
-    time.sleep(15)  
+        if uuid != '2e82ec5b72354497adfde07170979a72':
+            items = 0
+            continue
+        
+        itemCount = 0
+        p = itemCount
+        itemCount = p+1
+        gzipped_data = soldItem['item_bytes']
+        item = decode_inventory_data(gzipped_data)
+        print(f'Discovered {item} has sold. Profit was ')
+
+    print('starting loop')
+    time.sleep(29)
+    print('halfway\n')
+    time.sleep(29)
+    p = timesLooped
+    timesLooped = p+1
+    if timesLooped == 1:
+        print(f'DONE. The program has cycled {timesLooped} time')
+    else:
+        print(f'DONE. The program has cycled {timesLooped} times')

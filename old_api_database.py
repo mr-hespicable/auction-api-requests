210c64afb389c90c4975c7e48fe67c98f5fc380b
mr-hespicable <leonhmcqueen@gmail.com>
2023-01-02 16:29:33 +0000

    Update api_database.py and old_api_database.py and .gitignore

--- old_api_database.py
+++ old_api_database.py
@@ -1,8 +1,8 @@
-import time, io, requests, os, nbt, base64
+import io, requests, os, nbt, base64
 
 #thanks to ShadowMobX#0220 for refining this function. give him an internship
-USERNAME = input('paste username here\n')
-API_KEY = input('paste api key here\n')
+#USERNAME = input('paste username here\n')
+#API_KEY = input('paste api key here\n')
 
 def response(call):
     r = requests.get(call)
@@ -11,101 +11,93 @@ def response(call):
 def prettify(string):
     return ('{:,}'.format(string))
 
-class getApi:
-    uuid = response(f'https://api.mojang.com/users/profiles/minecraft/{USERNAME}')['id'] #uuid of the player
-    def getProfileID(self): #returns the profile ID of the player
-        for profile in response(f'https://api.hypixel.net/player?key={API_KEY}&uuid={self.uuid}')['player']['stats']['SkyBlock']['profiles']:
-            profileName = response(f'https://api.hypixel.net/player?key={API_KEY}&uuid={self.uuid}')['player']['stats']['SkyBlock']['profiles'][profile]['cute_name'] #skyblock profile name of the player
-            if profileName == PREFERRED_PROFILE:
-                profileID = response(f'https://api.hypixel.net/player?key={API_KEY}&uuid={self.uuid}')['player']['stats']['SkyBlock']['profiles'][profile]['profile_id']
-                return profileID #skyblock profile id of the player
+def checkProfile(uuid, api_key):
+        PREFERRED_PROFILE = input('paste name of active profile here\n')
+        profile_name_list = []
+        for profile in response(f'https://api.hypixel.net/player?key={api_key}&uuid={uuid}')['player']['stats']['SkyBlock']['profiles']:
+            name = response(f'https://api.hypixel.net/player?key={api_key}&uuid={uuid}')['player']['stats']['SkyBlock']['profiles'][profile]['cute_name']
+            profile_name_list.append(name)
+        if PREFERRED_PROFILE not in profile_name_list:
+            print(f'Not a valid profile: try again. Your profile options are {profile_name_list}')
+            checkProfile()
+        return PREFERRED_PROFILE
+
+class apiRequests:
+    def getProfileID(username, api_key, profile): #returns the profile ID of the player
+        uuid = response(f'https://api.mojang.com/users/profiles/minecraft/{username}')['id'] #uuid of the player
+        for profile in response(f'https://api.hypixel.net/player?key={api_key}&uuid={uuid}')['player']['stats']['SkyBlock']['profiles']:
+            profileName = response(f'https://api.hypixel.net/player?key={api_key}&uuid={uuid}')['player']['stats']['SkyBlock']['profiles'][profile]['cute_name'] #skyblock profile name of the player
+            if profileName == profile:
+                profileID = response(f'https://api.hypixel.net/player?key={api_key}&uuid={uuid}')['player']['stats']['SkyBlock']['profiles'][profile]['profile_id']
+                return profileID  #skyblock profile id of the player
             else:
                 continue
-    
-    def getPresentAuctions(self): #returns the player's auctions that are/were up on the auction house
-        return response(f'https://api.hypixel.net/skyblock/auction?key={API_KEY}&profile={z.getProfileID()}')['auctions']
-    
-    def getAuctionUID(self,url): #returns the UID of an auction
-        return response(url)['nbtData']['data']['uid']
-    
-    def getPastSales(self,uid): #returns the past sales of an auction, given the UID
-        url = f'https://sky.coflnet.com/api/auctions/uid/{uid}/sold'
+
+    def getPresentAuctions(api_key): #returns the player's auctions that are/were up on the auction house
+        return response(f'https://api.hypixel.net/skyblock/auction?key={api_key}&profile={getApi.getProfileID()}')['auctions']
+
+    def getAuctionUID(auctionUUID): #returns the UID of an auction
+        return response(f'https://sky.coflnet.com/api/auction/{auctionUUID}/uid')['nbtData']['data']['uid']
+
+    def getPastSales(uid): #returns the past sales of an auction, given the UID]
         return response(f'https://sky.coflnet.com/api/auctions/uid/{uid}/sold')
 
-    def getPastAuctionInfo(self,past):
-        url = f'https://sky.coflnet.com/api/auction/{past}'
-        return response(f'https://sky.coflnet.com/api/auction/{past}')
+    def getPastAuctionInfo(pastUUID):
+        return response(f'https://sky.coflnet.com/api/auction/{pastUUID}')
 
-    def getAuctionsSold(self,itemID):
-        url = f'https://sky.coflnet.com/api/auctions/tag/{itemID}/sold?page=1&pageSize=200'
+    def getAuctionsSold(itemID):
         return response(f'https://sky.coflnet.com/api/auctions/tag/{itemID}/sold?page=1&pageSize=200')
-z = getApi()
-
-class getEncoded:
-    def data(raw_data):
-        return nbt.nbt.NBTFile(fileobj=io.BytesIO(base64.b64decode(raw_data)))
-    def id(nbt_data):
-        extra = nbt_data.tags[0].tags[0]['tag']['ExtraAttributes']
-        return extra['id']
-    
-    def name(nbt_data):
-        z = nbt_data.tags[0].tags[0]['tag']['ExtraAttributes']
-        if str(z['id']) == 'PET':
-            petName = response(str(z['petInfo']))['type']
-            return petName
 
 
-def checkProfile():
-    global PREFERRED_PROFILE
-    PREFERRED_PROFILE = input('paste name of active profile here\n')
-    profile_name_list = []
-    for profile in response(f'https://api.hypixel.net/player?key={API_KEY}&uuid={z.uuid}')['player']['stats']['SkyBlock']['profiles']:
-        name = response(f'https://api.hypixel.net/player?key={API_KEY}&uuid={z.uuid}')['player']['stats']['SkyBlock']['profiles'][profile]['cute_name']
-        profile_name_list.append(name)
-    if PREFERRED_PROFILE not in profile_name_list:
-        print(f'Not a valid profile: try again. Your profile options are {profile_name_list}')
-        checkProfile()
-    return PREFERRED_PROFILE
-checkProfile()
+def data(raw_data):
+    return nbt.nbt.NBTFile(fileobj=io.BytesIO(base64.b64decode(raw_data)))
+def id(nbt_data):
+    extra = nbt_data.tags[0].tags[0]['tag']['ExtraAttributes']
+    return extra['id']
+def name(nbt_data):
+    z = nbt_data.tags[0].tags[0]['tag']['ExtraAttributes']
+    if str(z['id']) == 'PET':
+        petName = response(str(z['petInfo']))['type']
+        return petName
+
+getApi = apiRequests()
 
 profitList = []
 priceList = []
+for auctionItem in getApi.getPresentAuctions(): #finding price of item
+    sold = auctionItem['highest_bid_amount']
+    if sold == 0:
+        starting_bid = auctionItem['starting_bid']
+        itemName = auctionItem['item_name']
+        item_bytes = auctionItem['item_bytes']['data']
+        nbt_data = data(item_bytes)
+        auctionID = id(nbt_data)
+        auctionNameID = name(nbt_data)
+        if auctionID == 'PET':
+            itemID = f'PET_{auctionNameID}'
+        else:
+            itemID = auctionID
+        
+        auctionUUID = auctionItem['uuid']
+        
+        auctionUid = getApi.getAuctionUID(f'https://sky.coflnet.com/api/auction/{auctionUUID}/')
+        auctionPastSales = getApi.getPastSales(auctionUid)
 
-while True:
-    for auctionItem in z.getPresentAuctions(): #finding price of item
-        sold = auctionItem['highest_bid_amount']
-        if sold == 0:
-            starting_bid = auctionItem['starting_bid']
-            itemName = auctionItem['item_name']
-            item_bytes = auctionItem['item_bytes']['data']
-            nbt_data = getEncoded.data(item_bytes)
-            auctionID = getEncoded.id(nbt_data)
-            auctionNameID = getEncoded.name(nbt_data)
-            if auctionID == 'PET':
-                itemID = f'PET_{auctionNameID}'
-            else:
-                itemID = auctionID
-            
-            auctionUUID = auctionItem['uuid']
-            
-            auctionUid = z.getAuctionUID(f'https://sky.coflnet.com/api/auction/{auctionUUID}/')
-            auctionPastSales = z.getPastSales(auctionUid)
-
-            if auctionPastSales != []:    
-                for sale in auctionPastSales:
-                    if sale['buyer'] == z.uuid:
-                        boughtAuctionInfo = z.getPastSales(sale['uuid'])
-                        boughtAuctionRawPrice = boughtAuctionInfo['bids'][0]['amount']
-            else:
-                continue
-            auctionsSold = z.getAuctionsSold(itemID)
-            
-            for soldItem in auctionsSold:
-                uuid = soldItem['auctioneerId']
-                price = soldItem['highestBidAmount']
-            profit = (starting_bid - boughtAuctionRawPrice)*0.99
-            profitList.append(str(profit))
-            priceList.append(str(boughtAuctionRawPrice))
-            totalProfit = sum([float(x) for x in profitList])
-    totalCost = sum([float(y) for y in priceList])
-    os.system('clear')
+        if auctionPastSales != []:    
+            for sale in auctionPastSales:
+                if sale['buyer'] == uuid:
+                    boughtAuctionInfo = getApi.getPastSales(sale['uuid'])
+                    boughtAuctionRawPrice = boughtAuctionInfo['bids'][0]['amount']
+        else:
+            continue
+        auctionsSold = getApi.getAuctionsSold(itemID)
+        
+        for soldItem in auctionsSold:
+            uuid = soldItem['auctioneerId']
+            price = soldItem['highestBidAmount']
+        profit = (starting_bid - boughtAuctionRawPrice)*0.99
+        profitList.append(str(profit))
+        priceList.append(str(boughtAuctionRawPrice))
+        totalProfit = sum([float(x) for x in profitList])
+totalCost = sum([float(y) for y in priceList])
+os.system('clear')
